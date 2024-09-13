a9f7c24517bd879d57b2d76f679f5f8dd9d9ada9
Reed Monkey <shyaminreallife@gmail.com>
2024-09-13 23:45:10 +0545

    shapes and line

--- drawthing.py
+++ drawthing.py
@@ -1,16 +1,27 @@
 import cv2 as cv
 import numpy as np
 
-#create a blank image or space
+# Create a blank image or space
 blank = np.zeros((500,500,3), dtype='uint8')
 cv.imshow('Blank', blank)
 
-#paint  the image a certain color
+# Paint the image a certain color
 blank[200:300, 300:400] = 0, 225, 0
 cv.imshow('Green', blank)
 
-#2. draw a rectangle over it
-cv.rectangle(blank, (0,0), (225,225), (0,255,0), thickness =2)
+# Draw a rectangle over it
+cv.rectangle(blank, (0,0), (225,225), (0,255,0), thickness=cv.FILLED)
 cv.imshow('Rectangle', blank)
 
+# Draw a circle
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,225), thickness=-1)
+cv.imshow('Circle', blank)
+
+# Draw a line
+cv.line(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,0,225), thickness=3)
+cv.imshow('Line', blank)
+
+#write text
+cv.putText(blank, 'Hello', (225, 225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 225, 0), thickness=2)
+cv.imshow('Text', blank)
 cv.waitKey(0)
